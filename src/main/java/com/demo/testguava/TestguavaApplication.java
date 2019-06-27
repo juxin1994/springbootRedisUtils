8518c0d13807a0c3cab813b55823562385609334
juxin <834401462@qq.com>
2019-06-28 01:37:37 +0800

    dd

--- src/main/java/com/demo/testguava/TestguavaApplication.java
+++ src/main/java/com/demo/testguava/TestguavaApplication.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class TestguavaApplication {
 
     public static void main(String[] args) {
+        System.out.println("aaaa");
         SpringApplication.run(TestguavaApplication.class, args);
     }
 
